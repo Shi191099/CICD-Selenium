33cc7c3c1939b9c5cd40842f5629e71ca28dab47
your sasidharan57 <krishnakirit468@gmail.com>
2020-02-09 20:10:41 +0530

    chrome options added

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -24,9 +24,22 @@ public class FreeCrmTest {
 		System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");
 
 		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--headless");
-		chromeOptions.addArguments("--no-sandbox");
-		driver = new ChromeDriver(chromeOptions);
+		//chromeOptions.addArguments("--headless");
+		//chromeOptions.addArguments("--no-sandbox");
+		
+		  // ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
+        //AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+		chromeOptions.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+		chromeOptions.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+		chromeOptions.addArguments("--headless"); // only if you are ACTUALLY running headless
+		chromeOptions.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+		chromeOptions.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+		chromeOptions.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+		chromeOptions.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+		chromeOptions.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+        driver = new ChromeDriver(chromeOptions);
+		
+		//driver = new ChromeDriver(chromeOptions);
 
 		// driver = new ChromeDriver();
 		js = (JavascriptExecutor) driver;
