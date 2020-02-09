61f5d444eee7dc5353b8370f6c59b6c147a30f52
your sasidharan57 <krishnakirit468@gmail.com>
2020-02-09 19:58:17 +0530

    'latest'

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -21,15 +21,17 @@ public class FreeCrmTest {
 
 	@BeforeMethod
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", " /usr/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");
+
 		ChromeOptions chromeOptions = new ChromeOptions();
-             	chromeOptions.addArguments("--headless");
-             	chromeOptions.addArguments("--no-sandbox");
-		driver = new ChromeDriver(chromeOptions); 
-		}
-		//driver = new ChromeDriver();
+		chromeOptions.addArguments("--headless");
+		chromeOptions.addArguments("--no-sandbox");
+		driver = new ChromeDriver(chromeOptions);
+
+		// driver = new ChromeDriver();
 		js = (JavascriptExecutor) driver;
 		driver.get("https://www.freecrm.com/index.html");
+
 	}
 
 	@Test
@@ -69,13 +71,13 @@ public class FreeCrmTest {
 
 		// jquery-growl w/ no frills
 		js.executeScript("$.growl({ title: 'GET', message: '/' });");
-		
-		if(messageType.equals("error")){
-			js.executeScript("$.growl.error({ title: 'ERROR', message: '"+message+"' });");
-		}else if(messageType.equals("info")){
-			js.executeScript("$.growl.notice({ title: 'Notice', message: '"+message+"' });");
-		}else if(messageType.equals("warning")){
-			js.executeScript("$.growl.warning({ title: 'Warning!', message: '"+message+"' });");
+
+		if (messageType.equals("error")) {
+			js.executeScript("$.growl.error({ title: 'ERROR', message: '" + message + "' });");
+		} else if (messageType.equals("info")) {
+			js.executeScript("$.growl.notice({ title: 'Notice', message: '" + message + "' });");
+		} else if (messageType.equals("warning")) {
+			js.executeScript("$.growl.warning({ title: 'Warning!', message: '" + message + "' });");
 		}
 
 		// jquery-growl w/ colorized output
@@ -83,13 +85,13 @@ public class FreeCrmTest {
 //		js.executeScript("$.growl.notice({ title: 'Notice', message: 'your notice message goes here' });");
 //		js.executeScript("$.growl.warning({ title: 'Warning!', message: 'your warning message goes here' });");
 	}
-	
-	public static void takeScreenshot(String fileName) throws IOException{
+
+	public static void takeScreenshot(String fileName) throws IOException {
 		// Take screenshot and store as a file format
 		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		// now copy the screenshot to desired location using copyFile //method
-		FileUtils.copyFile(src, 
-				new File("/Users/NaveenKhunteta/Documents/MyPOMFramework/PageObjectModel/screenshots/" + fileName +".png"));
+		FileUtils.copyFile(src, new File(
+				"/Users/NaveenKhunteta/Documents/MyPOMFramework/PageObjectModel/screenshots/" + fileName + ".png"));
 
 	}
 
